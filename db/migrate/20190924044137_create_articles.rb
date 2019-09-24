22614c56780efef902232f23eaa169779236ab67
mtmtiger <mt.mtiger25347@gmail.com>
2019-09-24 17:10:58 +0900

    article_post_function

---
+++ db/migrate/20190924044137_create_articles.rb
@@ -0,0 +1,11 @@
+class CreateArticles < ActiveRecord::Migration[5.2]
+  def change
+    create_table :articles do |t|
+      t.string  :title,        null: false
+      t.string  :images
+      t.text    :text,         null: false
+      t.references :user,      null: false
+      t.timestamps
+    end
+  end
+end
