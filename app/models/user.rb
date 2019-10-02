b45514b4828e077845b661881765ce44a11cfb1e
mtmtiger <mt.mtiger25347@gmail.com>
2019-10-02 12:56:56 +0900

    follow_function

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,23 @@ class User < ApplicationRecord
   has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :passive_relationships, class_name:  "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followers, through: :passive_relationships, source: :follower
+
+    # ユーザーをフォローする
+    def follow(other_user)
+      active_relationships.create(followed_id: other_user.id)
+    end
+   
+    # ユーザーをアンフォローする
+    def unfollow(other_user)
+      active_relationships.find_by(followed_id: other_user.id).destroy
+    end
+   
+    # 現在のユーザーがフォローしてたらtrueを返す
+    def following?(other_user)
+      following.include?(other_user)
+    end
 end
