41e2c230f7f0790634dd140ad6cda1b58e56e93d
mtmtiger <mt.mtiger25347@gmail.com>
2019-10-08 13:56:22 +0900

    header_menu_arrange

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -4,4 +4,16 @@ $(document).on("turbolinks:load",function() {
     $(".main-header__menu-box__ul__lists__list").fadeIn(2000);
     $("#list-menu").fadeOut(300);
   })
+
+  $(function(){
+    let navPos = $("#header-nav").offset().top;
+
+    $(window).scroll(function(){
+      if($(window).scrollTop() > navPos){
+        $("#header-nav").css({"position":"fixed","top":0,"width":"100%"})
+      } else {
+        $("#header-nav").css("position", "static")
+      }
+    })
+  })
 })
